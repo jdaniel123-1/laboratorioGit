0b12a8b40b5a941bf17b31a47bcc2c89bba8f004
unknown <mariashe@unal.edu.co>
2026-05-10 22:20:04 -0500

    añadi las lineas de texto restantes mas la carpeta output con el documento de salida

---
+++ labgit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+    #include<fstream>
+    #include<sstream>
+    using namespace std;
+    int main() {
+        ifstream archivoEntrada("input/archivoEntrada.txt");
+        ofstream archivoSalida("output/archivoSalida.txt");
+        if (archivoEntrada.is_open()) {
+            cout<<"Archivo abierto"<<endl;
+            string linea;
+            while (getline(archivoEntrada, linea)) {
+            	archivoSalida << linea;
+            }
+            cout << "Archivo copiado correctamente";
+        }
+    }
+
